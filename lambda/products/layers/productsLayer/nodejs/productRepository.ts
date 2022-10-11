a73e5340c2f2343702bdc64946271bd6c4b83172
Brenno Araújo <brennoaraujo@yahoo.com>
2022-10-10 23:05:30 -0300

    refactor: productRepository

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -7,6 +7,7 @@ export interface Product {
   code: string;
   price: number;
   model: string;
+  productUrl: string;
 }
 
 export class ProductRepository {
@@ -71,12 +72,13 @@ export class ProductRepository {
         ConditionExpression: "attribute_exists(id)",
         ReturnValues: "UPDATED_NEW",
         UpdateExpression:
-          "set productName = :productName, code = :code, price = :price, model = :model",
+          "set productName = :productName, code = :code, price = :price, model = :model, productUrl = :productUrl",  
         ExpressionAttributeValues: {
           ":productName": product.productName,
           ":code": product.code,
           ":price": product.price,
           ":model": product.model,
+          ":productUrl": product.productUrl,
         },
       })
       .promise();
